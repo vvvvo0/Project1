c54a09dc136e4d98b48125ad48fb1db02aa3f1b0
vvvvo0 <idvvvvo@gmail.com>
2024-11-29 00:57:23 +0900

    1

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -1,7 +1,17 @@
+#define  _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
 int main(void)
 {
-	printf("Hello world! \n");
+	int num1, num2;
+	int result1, result2;
+
+	printf("두 개의 정수 입력:");
+	scanf("%d %d", &num1, &num2);
+
+	result1 = num1 - num2;
+	result2 = num1 * num2;
+	printf("%d - %d = %d\n", num1, num2, result1);
+	printf("%d X %d = %d", num1, num2, result2);
 	return 0;
 }
