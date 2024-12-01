a2f9cc7d334560c5a29f171a7e4e586a23891541
vvvvo0 <idvvvvo@gmail.com>
2024-12-02 02:39:02 +0900

    scanf함수 실수기반 입력 형태

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -3,10 +3,17 @@
 
 int main(void)
 {
-	int num1, num2;
-	printf("두 개의 정수 입력: ");
-	scanf("%d %d", &num1, &num2);
-	printf("%d - %d = %d\n", num1, num2, num1 - num2);
-	printf("%d X %d = %d", num1, num2, num1 * num2);
+	float num1;
+	double num2;
+	long double num3;
+	printf("실수 입력1(e 표기법으로): ");
+	scanf("%f", &num1);
+	printf("입력된 실수 %f\n", num1);
+	printf("실수 입력2(e 표기법으로): ");
+	scanf("%lf", &num2);
+	printf("입력된 실수 %lf\n", num2);
+	printf("실수 입력3(e 표기법으로): ");
+	scanf("%Lf", &num3);
+	printf("입력된 실수 %Lf", num3);
 	return 0;
 }
