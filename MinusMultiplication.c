4463c89e90cf6db45889804f9af323c6c08d2945
vvvvo0 <idvvvvo@gmail.com>
2024-12-02 03:31:19 +0900

    구구단에서 변수 초기화 필요성 확인

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -3,15 +3,14 @@
 
 int main(void)
 {
-	int num;
-	int i=0;
-	printf("입력 받은 양의 정수: ");
-	scanf("%d", &num);
+	int dan, num = 1;
+	printf("몇 단? ");
+	scanf("%d", &dan);
 	
-	while (i<num)
+	while (num<10)
 	{
-		printf("Hello world! \n", num);
-		i++;
+		printf("%d X %d = %d\n", dan, num, dan * num);
+		num++;
 	}
 	return 0;
 }
