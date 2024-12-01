e84c5bc7cafcd3eb71bd2b3fd71a4d9a99a1c5cd
vvvvo0 <idvvvvo@gmail.com>
2024-12-02 03:02:36 +0900

    NineNineDan.c

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -3,17 +3,14 @@
 
 int main(void)
 {
-	float num1;
-	double num2;
-	long double num3;
-	printf("실수 입력1(e 표기법으로): ");
-	scanf("%f", &num1);
-	printf("입력된 실수 %f\n", num1);
-	printf("실수 입력2(e 표기법으로): ");
-	scanf("%lf", &num2);
-	printf("입력된 실수 %lf\n", num2);
-	printf("실수 입력3(e 표기법으로): ");
-	scanf("%Lf", &num3);
-	printf("입력된 실수 %Lf", num3);
+	int dan=0, num = 1;
+	printf("몇 단? ");
+	scanf("%d", &dan);
+	
+	while (num < 10)
+	{
+		printf("%d X %d = %d\n", dan, num, dan * num);
+		num++;
+	}
 	return 0;
 }
