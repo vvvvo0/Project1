b560394a1164702e1e0a4b8da807cc975222cae8
vvvvo0 <idvvvvo@gmail.com>
2024-12-02 19:26:51 +0900

    while문 중첩(2)

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -3,20 +3,23 @@
 #include<stdio.h>
 
 int main(void) {
-  
-    int outnum=2; // 단을 나타내는 변수
-    int innum=1; //1부터 9까지 곱
-
-        while(outnum < 10) //2부터 9까지 반복
-        {
-            innum = 1; //다시 1부터 곱해주기 위해 초기화 필요함.
-            while (innum<10)
-            {
-                printf("%d x %d = %d\n", outnum, innum, outnum*innum);
-                innum++;
-            }
-         outnum++;
-        }
-       
+    
+    int i = 0; //입력할 정수 개수
+    int num = 0; //입력한 정수 저장
+    int sum = 0; //입력한 정수의 합 저장
+   
+    while (i < 5) //5개 입력할 때까지 반복
+    {
+        printf("%d개 입력 중입니다\n", i + 1);
+        scanf("%d", &num);
+          while (num<1) //1 미만의 정수가 입력되는 경우 재 입력 요구
+          {
+            printf("다시 입력하세요.\n");
+            scanf("%d", &num); //재입력 받기
+          }
+        sum += num; //안쪽while문 이후에 위치
+        i++;
+    }
+    printf("입력한 5개 정수의 합: %d", sum);
     return 0;
 }
