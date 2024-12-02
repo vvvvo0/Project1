2ce5e3b1cce297202f41418838fd4c17efc8cc42
vvvvo0 <idvvvvo@gmail.com>
2024-12-02 19:47:03 +0900

    while문 중첩(2)

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -5,21 +5,20 @@
 int main(void) {
     
     int i = 0; //입력할 정수 개수
-    int num = 0; //입력한 정수 저장
-    int sum = 0; //입력한 정수의 합 저장
+    int j = 0; //입력한 정수 저장
    
-    while (i < 5) //5개 입력할 때까지 반복
+    while (i < 5) //5번 반복
     {
-        printf("%d개 입력 중입니다\n", i + 1);
-        scanf("%d", &num);
-          while (num<1) //1 미만의 정수가 입력되는 경우 재 입력 요구
+        j = 0;
+          while (j<i) //o
           {
-            printf("다시 입력하세요.\n");
-            scanf("%d", &num); //재입력 받기
+      
+            printf("o");
+       
+            j++;
           }
-        sum += num; //안쪽while문 이후에 위치
+        printf("*\n");
         i++;
     }
-    printf("입력한 5개 정수의 합: %d", sum);
     return 0;
 }
