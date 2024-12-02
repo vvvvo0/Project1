2490370fe76c9a73a9ed23f73f6247e1868b6a6f
vvvvo0 <idvvvvo@gmail.com>
2024-12-02 18:06:41 +0900

    입력 받은 정수의 평균을 출력하는 프로그램. 형 변환.

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -2,26 +2,26 @@
 
 #include<stdio.h>
 
-int main(void)
-{
-	int num = 0;
-	int i = 9; //역순으로 출력하려면 9부터 시작해야 하므로 i을 9로 초기화
+int main(void) {
+    int count;
+    int num;
+    int sum = 0;
+    float average; // 소수점 이하까지 계산하기 위해 float 자료형으로 선언
+    int i = 0;
 
-	
+    printf("몇 개의 정수를 입력하시겠습니까? ");
+    scanf("%d", &count);
 
-	while (i>0) //역순으로 출력하기 위해서 num이 0보다 클 때까지 반복
-	{
-		printf("몇 단?: ");
-		scanf("%d", &num);
-		printf("%d x %d = %d \n", num, i, num*i);
-		i--; //num값 감소
-	}
-	return 0;
+    while (i < count) 
+    {
+        printf("%d번째 정수를 입력하세요: ", i + 1);
+        scanf("%d", &num);
+        sum += num;
+        i++;
+    }
+
+    average = (double)sum / count; // sum을 double로 형변환
+    printf("입력된 정수의 평균값은 %f입니다.\n", average);
+
+    return 0;
 }
-
-/*직전 코드의 차이점은 printf("몇 단?: "); 와 scanf("%d", &num); 의 위치.
-첫 번째 코드에서는 while 문 밖에서 단 한 번만 입력을 받지만, 두 번째 코드에서는 while 문 안에서 매번 입력을 받음.
-while 문 안에서 입력을 받게 되면, 반복문이 실행될 때마다 사용자 입력을 기다리게 됨.
- 즉, 9단부터 1단까지 출력되는 동안 매번 "몇 단?: "이 출력되고 사용자 입력을 기다리기 때문에 한 줄씩 출력되는 듯.
- 구구단 전체를 한 번에 출력하려면 첫 번째 코드처럼 while 문 밖에서 단 한 번만 입력을 받아야 함.
-*/
