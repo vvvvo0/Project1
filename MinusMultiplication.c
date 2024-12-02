a50f8b83366effbc1e9971ca7c19ba4a228a3bb2
vvvvo0 <idvvvvo@gmail.com>
2024-12-02 18:45:05 +0900

    while문 중첩

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -3,25 +3,20 @@
 #include<stdio.h>
 
 int main(void) {
-    int count;
-    int num;
-    int sum = 0;
-    float average; // 소수점 이하까지 계산하기 위해 float 자료형으로 선언
-    int i = 0;
+  
+    int outnum=2; // 단을 나타내는 변수
+    int innum=1; //1부터 9까지 곱
 
-    printf("몇 개의 정수를 입력하시겠습니까? ");
-    scanf("%d", &count);
-
-    while (i < count) 
-    {
-        printf("%d번째 정수를 입력하세요: ", i + 1);
-        scanf("%d", &num);
-        sum += num;
-        i++;
-    }
-
-    average = (double)sum / count; // sum을 double로 형변환
-    printf("입력된 정수의 평균값은 %f입니다.\n", average);
-
+        while(outnum < 10) //2부터 9까지 반복
+        {
+            innum = 1; //다시 1부터 곱해주기 위해 초기화 필요함.
+            while (innum<10)
+            {
+                printf("%d x %d = %d\n", outnum, innum, outnum*innum);
+                innum++;
+            }
+         outnum++;
+        }
+       
     return 0;
 }
