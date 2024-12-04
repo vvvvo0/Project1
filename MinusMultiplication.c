9d76c3fd0d650417b021efd4139f061c4a395b17
vvvvo0 <idvvvvo@gmail.com>
2024-12-04 18:47:05 +0900

    입력한 영단어를 구성하는 문자 중에서 아스키 코드의 값이 가장 큰 문자를 찾아서 출력하는 프로그램.

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -7,22 +7,24 @@ int main(void)
     char str[50];
     int arrlen = 0;
     int i;
+    char max=0;
 
     printf("입력한 문자열:");
     scanf("%s", str); //입력 받은 문자열을 배열str에 저장.
-    
-    //paris > sirap 끝까지 구하고, 그 끝에서 부터 출력. 
+
 
     while (str[arrlen] != '\0') // arrlen이 가리키는 문자가 널 문자 (\0)가 아닌 경우 참.
     {  
-        arrlen++; // 반복문이 한 번 실행될 때마다 arrlen 값을 1씩 증가.
+        arrlen++; 
     } 
-    //while 문은 조건식이 참인 동안 반복적으로 실행
+    //단어 길이 계산
     
-    printf("역순 출력: ");
-    for (i = arrlen-1; i >= 0;i--) {
-        printf("%c", str[i]);
+    for (i = 0 ; i<arrlen ; i++) {
+        if (max < str[i])
+            max = str[i];
+        
     }
 
+    printf("%c", max);
     return 0;
 }
