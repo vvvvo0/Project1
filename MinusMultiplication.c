1e9a354335b8c02dccf1dec5bad57fa4d688ceb8
vvvvo0 <idvvvvo@gmail.com>
2024-12-04 17:36:23 +0900

    배열(1)

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -4,26 +4,16 @@
 
 int main(void) 
 {
+    int i;
+    char arr[] = { 'G','o','o','d' };
+    int arrlen;
     
-    int num1 = 10;
-    int num2 = 20;
+    arrlen = sizeof(arr) / sizeof(char);
 
-    int* ptr1; //포인터 변수를 선언만하고 초기화하지 않으면 쓰레기 값으로 초기화 됨.
-               //에러메시지: 초기화되지 않은 ptr1 지역 변수를 사용했습니다.
-               //에러메시지: 초기화되지 않은 메모리 ptr1을 사용하고 있습니다.
-    int* ptr2 = &num2;
-    int* temp;
+    for (i = 0;i < arrlen;i++) {
 
+        printf("%d", arr[i]);
 
-    (*ptr1) += 10;
-    (*ptr2) -= 10;
-
-    temp = ptr1;
-    ptr1 = ptr2;
-    ptr2 = temp; //포인터 변수 ptr1 가리키는 변수와 ptr2가 가리키는 변수를 바꾸기 위해 포인터 변수 하나 추가.
-
-    printf("%d, %d", *ptr1, *ptr2);
-
-
+    }
     return 0;
 }
