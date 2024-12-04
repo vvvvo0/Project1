60b149823e0889791828e37b6a64f21f73dfc0bc
vvvvo0 <idvvvvo@gmail.com>
2024-12-04 17:03:30 +0900

    포인터 변수(2)

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -10,16 +10,17 @@ int main(void)
 
     int* ptr1 = &num1;
     int* ptr2 = &num2;
+    int* temp;
+
 
     (*ptr1) += 10;
     (*ptr2) -= 10;
 
-    ptr1 = &num2;
-    ptr2 = &num1;
+    temp = ptr1;
+    ptr1 = ptr2;
+    ptr2 = temp; //포인터 변수 ptr1 가리키는 변수와 ptr2가 가리키는 변수를 바꾸기 위해 포인터 변수 하나 추가.
 
-    printf("ptr1: %d, ptr2: %d", *ptr1, *ptr2);
-
-
+    printf("%d, %d", *ptr1, *ptr2);
 
 
     return 0;
