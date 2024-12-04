57660c4d3787b291da6a7c93bb1d6c93995b09ac
vvvvo0 <idvvvvo@gmail.com>
2024-12-04 18:06:11 +0900

    입력한 영단어 길이 출력 프로그램

--- MinusMultiplication.c
+++ MinusMultiplication.c
@@ -4,16 +4,20 @@
 
 int main(void) 
 {
-    int i;
-    char arr[] = { 'G','o','o','d' };
-    int arrlen;
+    char str[50];
+    int arrlen = 0;
+
+    printf("입력한 문자열: %s", str);
+    scanf("%s", str); //입력 받은 문자열을 배열str에 저장.
     
-    arrlen = sizeof(arr) / sizeof(char);
 
-    for (i = 0;i < arrlen;i++) {
 
-        printf("%c", arr[i]); //문자로 출력을 위해 서식을 %c로 변경.
-
-    }
+    while (str[arrlen] != '\0') // arrlen이 가리키는 문자가 널 문자 (\0)가 아닌 경우 참.
+    {  
+        arrlen++; // 반복문이 한 번 실행될 때마다 arrlen 값을 1씩 증가.
+    } 
+    //while 문은 조건식이 참인 동안 반복적으로 실행
+    
+    printf("영단어 길이: %d", arrlen);
     return 0;
 }
